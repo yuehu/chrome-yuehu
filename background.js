c3208366d6a63f108bed3586020410014e31ea31
Hsiaoming Yang <me@lepture.com>
2013-09-06 20:58:17 +0800

    Add context menu

--- background.js
+++ background.js
@@ -99,6 +99,20 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
   });
 });
 
+/**
+ * Create context menus.
+ */
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  record(info.linkUrl);
+});
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    id: 'yuehu',
+    title: i18n('name'),
+    contexts: ['link']
+  });
+});
+
 
 function noop() {};
 
