65d61842042d328becbdc22998c35d71d5d641a7
Hsiaoming Yang <me@lepture.com>
2013-09-08 09:27:47 +0800

    Move zhihu to site

--- index.js
+++ index.js
@@ -6,48 +6,6 @@
 
 
 /**
- * Support for zhihu.com
- */
-var ZHIHU_PATTERN = /www\.zhihu\.com\/question\/\d+/;
-var ZHIHU_CACHE = {};
-function zhihu() {
-  var m = location.href.match(/www\.zhihu\.com\/question\/(\d+)/);
-  if (!m) return;
-
-  var question = m[1];
-
-  var items = document.querySelectorAll('.zm-item-answer');
-
-  var createIcon = function(item) {
-    var token = item.dataset.atoken;
-    if (!token || ZHIHU_CACHE[token] || len(item.innerText) < 200) {
-      return;
-    }
-    ZHIHU_CACHE[token] = true;
-
-    var anchor = document.createElement('a');
-    anchor.href = 'javascript:;';
-    anchor.className = 'meta-item';
-    anchor.innerHTML = '<i class="z-icon-collect"></i>' + i18n('name');
-
-    anchor.onclick = function(e) {
-      e.preventDefault();
-      readlater('http://www.zhihu.com/question/' + question + '/answer/' + token);
-    };
-    var position = item.querySelector('.zu-autohide');
-    position.parentNode.insertBefore(anchor, position);
-  };
-
-  for (var i = 0; i < items.length; i++) {
-    createIcon(items[i]);
-  }
-}
-if (ZHIHU_PATTERN.test(location.href)) {
-  observer(zhihu);
-}
-
-
-/**
  * Observe the DOM changes.
  */
 function observer(callback) {
---
+++ site/zhihu.js
@@ -0,0 +1,40 @@
+/**
+ * Add yuehu button on www.zhihu.com
+ */
+var ZHIHU_PATTERN = /www\.zhihu\.com\/question\/\d+/;
+var ZHIHU_CACHE = {};
+function zhihu() {
+  var m = location.href.match(/www\.zhihu\.com\/question\/(\d+)/);
+  if (!m) return;
+
+  var question = m[1];
+
+  var items = document.querySelectorAll('.zm-item-answer');
+
+  var createIcon = function(item) {
+    var token = item.dataset.atoken;
+    if (!token || ZHIHU_CACHE[token] || len(item.innerText) < 200) {
+      return;
+    }
+    ZHIHU_CACHE[token] = true;
+
+    var anchor = document.createElement('a');
+    anchor.href = 'javascript:;';
+    anchor.className = 'meta-item';
+    anchor.innerHTML = '<i class="z-icon-collect"></i>' + i18n('name');
+
+    anchor.onclick = function(e) {
+      e.preventDefault();
+      readlater('http://www.zhihu.com/question/' + question + '/answer/' + token);
+    };
+    var position = item.querySelector('.zu-autohide');
+    position.parentNode.insertBefore(anchor, position);
+  };
+
+  for (var i = 0; i < items.length; i++) {
+    createIcon(items[i]);
+  }
+}
+if (ZHIHU_PATTERN.test(location.href)) {
+  observer(zhihu);
+}
